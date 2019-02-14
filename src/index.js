d16eadc6bb492d4f3fdc1f8edf4cfa8289b266fe
engreric <okemmadueric@gmail.com>
2019-02-14 18:29:34 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import FormContainer from "./components/container/FormContainer.js";
+
+ReactDOM.render(
+    <div >
+        <FormContainer />
+    </div>,
+    document.getElementById('app')
+);
